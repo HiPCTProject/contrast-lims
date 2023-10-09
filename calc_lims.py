342ad7ee477d29578f076f4ab63eeb3bb73a0e35
David Stansby <dstansby@gmail.com>
2023-10-09 14:12:14 +0100

    Add pre-commit

--- calc_lims.py
+++ calc_lims.py
@@ -1,14 +1,18 @@
-from hipct_data_tools import load_datasets
-import numpy as np
-import glymur
-from pathlib import Path
 from multiprocessing import Pool
+from pathlib import Path
+from typing import Any
 
+import glymur
+import numpy as np
+import numpy.typing as npt
 from tqdm import tqdm
+
+from hipct_data_tools import load_datasets
 
 HIST_PATH = Path("/data/projects/hop/data_repository/Various/histograms")
 
-def data_in_circle(arr):
+
+def data_in_circle(arr: npt.NDArray[Any]) -> npt.NDArray[Any]:
     """
     Given a 2D array, return only an array inside a circle touching the edges.
     """
@@ -17,12 +21,14 @@ def data_in_circle(arr):
     mask = (X**2 + Y**2) < 0.25
     return arr[mask]
 
-def value_count(jp2_path):
+
+def value_count(jp2_path: Path) -> npt.NDArray[np.uint64]:
     jp2k = glymur.Jp2k(jp2_path)
     all_data = jp2k[:]
     data = data_in_circle(all_data)
     counts = np.bincount(data, minlength=2**16)
     return counts.astype(np.uint64)
+
 
 if __name__ == "__main__":
     datasets = load_datasets()
@@ -30,6 +36,11 @@ if __name__ == "__main__":
     hist_data = np.zeros(2**16, dtype=np.uint64)
 
     for dataset in datasets:
+        hist_path = HIST_PATH / f"counts_{dataset.name}.npz"
+        if hist_path.exists():
+            print(f"Already processed {dataset.name}")
+            continue
+
         step = 10
         paths = sorted(dataset.esrf_jp2_path.glob("*.jp2"))[::step]
         print(f"Processing {dataset.name}...")
